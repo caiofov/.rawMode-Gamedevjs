bef610a65ae9f44f25e793c22f879fad8fda5364
caiofov <cfoviana@gmail.com>
2022-04-17 13:06:42 -0300

    start scene animation

--- js/scenes/Preloader.js
+++ js/scenes/Preloader.js
@@ -8,6 +8,8 @@ class Preloader extends Phaser.Scene{
         this.load.image('help_menu', '../../assets/img/helpmenu.png')
 
         this.load.spritesheet('coin', '../../assets/img/coin.png', {frameWidth:36, frameHeight:40})
+        this.load.spritesheet('binary', '../../assets/img/binary.png', {frameWidth:800, frameHeight:450})
+
     }
     create(){
         this.anims.create({
@@ -18,6 +20,15 @@ class Preloader extends Phaser.Scene{
             }),
             frameRate: 20,
             repeat: -1,
+        })
+        this.anims.create({
+            key:'binary_background',
+            frames: this.anims.generateFrameNumbers('binary',{
+                start: 0,
+                end: 29
+            }),
+            frameRate: 12,
+            repeat: 0
         })
 
         this.scene.start('StartScene')
--- js/scenes/StartScene.js
+++ js/scenes/StartScene.js
@@ -3,14 +3,22 @@ class StartScene extends Phaser.Scene{
         super('StartScene')
     }
     create(){
-        this.timer = 0
+        this.background = this.add.sprite(0,0,'binary').setOrigin(0)
+        this.background.displayHeight = game.config.height
+        this.background.anims.play('binary_background')
+
         this.startText = this.add.text(game.config.width/2, game.config.height/2, 'START', {fontSize: '32px'})
         .setOrigin(.5)
         .setInteractive()
+        
+        this.startText.visible = false
 
+        
     }
     update(){
-
+        if(!this.background.anims.isPlaying){
+            this.startText.visible = true
+        }
         this.startText.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, () =>{
             if(!this.isMenuOpen){
                 this.startText.setScale(1.2);
