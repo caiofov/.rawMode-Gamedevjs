712d6b7a4186557a2356b597faa756384268f7b7
caiofov <cfoviana@gmail.com>
2022-04-22 19:53:35 -0300

    animate title

--- js/scenes/StartScene.js
+++ js/scenes/StartScene.js
@@ -11,12 +11,27 @@ class StartScene extends Phaser.Scene{
         .setOrigin(.5)
         .setInteractive()
         .setVisible(false)
+
+        this.title = this.add.text(game.config.width/2, 80, "", {fontSize: '60px'})
+        .setOrigin(.5)
+        .setAlign('center') 
+        .setVisible(false)
+
+        this.temporary_title = ""
+        this.title_characters = (".raw mode").split('')
+        this.count = 0
         
     }
     update(){
         if(!this.background.anims.isPlaying){
             this.startText.setVisible(true)
+            this.title.setVisible(true)
+        
+            if(this.count < this.title_characters.length *2){
+                this.animateTitle()
+            }
         }
+
         this.startText.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, () =>{
             this.startText.setScale(1.2);
         })
@@ -27,6 +42,25 @@ class StartScene extends Phaser.Scene{
         this.startText.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () =>{
             this.scene.start('LevelMenu')
         })
+        
+    }
+
+    animateTitle(){
+
+        if(this.temporary_title.length < 9){
+            this.temporary_title += Math.random() > 0.5 ? 1 : 0
+        }
+        else if(this.count < 9){
+            let replace = this.temporary_title.charAt(this.count) == 0 ? 1 : 0
+            this.temporary_title = this.temporary_title.substring(0,this.count) + replace + this.temporary_title.substring(this.count+1)
+            this.count++
+        }
+        else{
+            let idx = this.count - 9
+            this.temporary_title = this.temporary_title.substring(0,idx) + this.title_characters[idx]+ this.temporary_title.substring(idx+1)
+            this.count++
+        }
+        this.title.setText(this.temporary_title)
         
     }
 
