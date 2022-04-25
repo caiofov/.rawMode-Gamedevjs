23372b8a3d3cb03e088e562370738c0da20f70dd
caiofov <cfoviana@gmail.com>
2022-04-25 16:45:49 -0300

    fixing directories

--- js/scenes/Preloader.js
+++ js/scenes/Preloader.js
@@ -14,12 +14,12 @@ class Preloader extends Phaser.Scene{
         //water block
         this.load.image('water', 'assets/img/water.png')
         // cloud block
-        this.load.image('cloud', '../../assets/img/cloud.png')
+        this.load.image('cloud', 'assets/img/cloud.png')
         //bits
-        this.load.image('bit0', '../../assets/img/bit0.png')
-        this.load.image('bit1', '../../assets/img/bit1.png')
-        this.load.image('gray0', '../../assets/img/gray0.png')
-        this.load.image('gray1', '../../assets/img/gray1.png')
+        this.load.image('bit0', 'assets/img/bit0.png')
+        this.load.image('bit1', 'assets/img/bit1.png')
+        this.load.image('gray0', 'assets/img/gray0.png')
+        this.load.image('gray1', 'assets/img/gray1.png')
 
         //help button (question mark)
         this.load.spritesheet('question', 'assets/img/question.png', {frameWidth:16, frameHeight:16})
@@ -28,11 +28,11 @@ class Preloader extends Phaser.Scene{
         // https://www.videvo.net/video/binary-numbers-01-alpha-matte/1343/ -> video
         
         //player spritesheet
-        this.load.spritesheet('player', '../assets/img/player.png', {frameWidth:32, frameHeight:32})
+        this.load.spritesheet('player', 'assets/img/player.png', {frameWidth:32, frameHeight:32})
         //raw player spritesheet
-        this.load.spritesheet('player_raw', '../assets/img/player_raw.png', {frameWidth:32, frameHeight:32})
+        this.load.spritesheet('player_raw', 'assets/img/player_raw.png', {frameWidth:32, frameHeight:32})
         //finish line
-        this.load.spritesheet('finish_line', '../assets/img/finishline.png', {frameWidth:32, frameHeight:32})
+        this.load.spritesheet('finish_line', 'assets/img/finishline.png', {frameWidth:32, frameHeight:32})
     
     }
     create(){
